5faf4732814d8050b5ceff1421e4602636f53b45
Moustafa Elsisy <Moustafa Elsisy>
2017-03-10 17:39:56 -0330

    Added global SASS partials

--- index.php
+++ index.php
@@ -33,7 +33,7 @@
     <div id="section-2" class="section">
       <?php echo'<h2 class="text-center">All what I have been up to recently is right here <img src="'.$IMAGE_DIR.'curv_arrow4.png"></img></h2>'?>
     </div>
-    <div id="section-3" class="section tile" style="background-color: #374353;">
+    <div id="section-3" class="section tile">
       <div class="container">
         <div class="row">
           <div class="col-md-4" style="border-color: #2B3440;">
@@ -57,7 +57,31 @@
           </div>
         </div>
       </div>
-      <div>
+    </div>
+    <div id="section-4" class="section tile">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4" style="border-color: #2B3440;">
+            <div style="margin: 0 auto;">
+              <span class="fa-stack fa-5x">
+                <i class="fa fa-square-o fa-stack-2x"></i>
+                <i class="fa fa-th-list fa-stack-1x"></i>
+              </span>
+            </div>
+            <div style="margin:0 auto;" class="btn-round">
+              See Projects
+            </div>
+          </div>
+          <div class="col-md-8">
+            <h6>Projects</h6>
+            <p>I have had a few enlightening experiences in a variety of fields,
+              ranging from desktop gamemode scripting to web development. Some of
+              the projects I have made are closed source, but a preview of these
+              programs is available over at my projects webpage, along with other
+              open source projects and their GitHub repositories :)
+          </div>
+        </div>
+      </div>
     </div>
   </body>
 </html>
