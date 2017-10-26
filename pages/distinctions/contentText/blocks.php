6b29ed29ec5acdb3926956699b870ba1bd4958a6
moustafaelsisy <spaceflyby@gmail.com>
2017-10-25 23:42:32 -0230

    * Added the distinctions.php file
    * Added the distinctions blocks to the distinctions page

---
+++ pages/distinctions/contentText/blocks.php
@@ -0,0 +1,12 @@
+<?php
+require_once ($_SERVER['DOCUMENT_ROOT']."/pages/common/Block.php");
+
+$blocks = array(
+  new Block("Memorial University International Undergraduate Academic Award 2017-2018", ""),
+  new Block("Faculty of Science Dean's List 2016-2017", ""),
+  new Block("Winning Team at Hacking Health, St. John's 2017", ""),
+  new Block("Memorial University International Student Recognition Awards Nominee 2016", ""),
+  new Block("Memorial University International Student Entrance Academic Scholarship", "")
+);
+
+ ?>
---
+++ pages/distinctions/distinctions.php
@@ -0,0 +1,27 @@
+<?php
+ require_once ($_SERVER['DOCUMENT_ROOT']."/includes/head.php");
+ require_once ("contentText/blocks.php");
+ require_once ($_SERVER['DOCUMENT_ROOT']."/pages/common/Blocks.php");
+ require_once ($_SERVER['DOCUMENT_ROOT']."/pages/common/TopNav.php");
+ require_once ($_SERVER['DOCUMENT_ROOT']."/pages/common/Footer.php");
+ ?>
+
+<!DOCTYPE HTML>
+<html>
+  <head>
+    <title>Moustafa Elsisy</title>
+    <?php
+      echo $GLOBAL_HEAD;
+      echo '
+      <link rel="stylesheet" type="text/css" href="/'.getCSS(__FILE__).'">
+      <script src="/'.getJS(__FILE__).'" type="text/javascript"></script>';
+    ?>
+  </head>
+  <body>
+    <?php
+      echo (new TopNav())->view();
+      echo (new Blocks($blocks))->view();
+      echo (new Footer())->view();
+     ?>
+  </body>
+</html>
