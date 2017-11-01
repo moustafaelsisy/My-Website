e3c1b15af61f0c4563d3ccaa597505aca2ae6032
moustafaelsisy <spaceflyby@gmail.com>
2017-11-01 08:31:18 -0230

    Moved the homepage's brief to its own component

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
  require_once ("pages/index/contentText/tiles.php");
  require_once ($_SERVER['DOCUMENT_ROOT']."/pages/common/TopNav.php");
  require_once ($_SERVER['DOCUMENT_ROOT']."/pages/index/components/Header.php");
+ require_once ($_SERVER['DOCUMENT_ROOT']."/pages/index/components/Brief.php");
  require_once ($_SERVER['DOCUMENT_ROOT']."/pages/index/components/PreFooter.php");
  require_once ($_SERVER['DOCUMENT_ROOT']."/pages/common/Footer.php");
 
@@ -23,11 +24,7 @@
     <?php
     echo (new TopNav())->view();
     echo (new Header())->view();
-    ?>
-    <div id="section-2" class="section">
-      <?php echo'<h2 class="text-center">All what I have been up to recently is right here <img src="'.$IMAGE_DIR.'curv_arrow4.png"></img></h2>'?>
-    </div>
-    <?php
+    echo (new Brief())->view();
       //Generate the tiles
       foreach($tiles as $tile){
         echo $tile->view();
