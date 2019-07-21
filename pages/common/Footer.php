564bc22f15dc5ce8b303193893d66fc19c26d366
moustafaelsisy <spaceflyby@gmail.com>
2019-07-21 11:01:56 -0700

    Updated distinctions to include recent awards

--- pages/common/Footer.php
+++ pages/common/Footer.php
@@ -3,7 +3,7 @@
 class Footer{
   public function view(){
     return '
-    <div id="footer">&copy; Moustafa Elsisy 2017-2018. All Rights Reserved.
+    <div id="footer">&copy; Moustafa Elsisy 2017-2019. All Rights Reserved.
     </div>
     ';
   }
--- pages/distinctions/contentText/blocks.php
+++ pages/distinctions/contentText/blocks.php
@@ -11,20 +11,16 @@ $blocks = array(
      The Book Prize is awarded on the basis of demonstrated academic excellence in the declared major to a student who is not graduating in the academic year of the award."
    ),
   new Block(
-    "Faculty of Science Dean's List 2017-2018, 2016-2017",
+    "Faculty of Science Dean's List 2018-2019, 2017-2018, 2016-2017",
     "The top achieving 10% of the Faculty of Science are named to the Faculty's Dean's List, in order to be awarded at a ceremony by the Dean."
   ),
   new Block(
-    "Memorial University Golf Tournament Scholarship 2018-2019",
-     "Awarded to ~7 Memorial University students per year, the scholarship is raised through the annual Memorial University Scholarship Golf Tournament."
+    "Memorial University International Undergraduate Academic Award  2018-2019, 2017-2018, 2016-2017",
+     "This award is given to the top achieving international undergraduate students across all faculties, in the previous academic year"
    ),
   new Block(
     "Winning Team at Startup Weekend NL 2017",
      "Worked on OceanWeb: An IoT network off NL's east coast , that utilises chip-enabled buoys in order to enhance data collection and threat mitigation for off-shore oil rigs."
-   ),
-  new Block(
-    "Memorial University International Undergraduate Academic Award 2017-2018",
-     "This award is given to the top achieving international undergraduate students across all faculties, in the previous academic year"
    ),
   new Block(
     "Winning Team at Hacking Health, St. John's 2017",
@@ -32,6 +28,10 @@ $blocks = array(
     For 48 hours, I worked along with my team on PatientMe: A web-app that we had the honour to present to Newfoundland's Minister of Health after his invitation.
     PatientMe focuses on helping the public find their healthcare through smart matchmaking with available Doctors.
     The demo also utilized tools such as the Google Maps API and Google Translate, to allow patients to easily find and navigate to their desired doctor, while eliminating linguistic boundaries."),
+  new Block(
+    "Memorial University Golf Tournament Scholarship 2018-2019",
+      "Awarded to ~7 Memorial University students per year, the scholarship is raised through the annual Memorial University Scholarship Golf Tournament."
+    ),
   new Block(
     "Memorial University International Student Entrance Academic Scholarship",
     "This scholarship is given to highly achieving entering International undergraduate students in light of their high school achievements and engagement")
